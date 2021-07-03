a177d2de162d1753b4f5254e02d1f261840def63
ixhbinphoenix <47122082+ixhbinphoenix@users.noreply.github.com>
2021-07-03 13:10:41 +0200

    Added stream command

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -16,6 +16,11 @@ interface TokenConfig {
     expiresIn: number,
     obtainmentTimestamp: number
 }
+export interface StreamData {
+    title: string,
+    date: string,
+    time: string
+}
 
 export function getConfig(): botConfig {
         return JSON.parse(fs.readFileSync("./src/config.json").toString());
@@ -25,4 +30,11 @@ export function getAPITokens(): TokenConfig {
 }
 export function getTMITokens(): TokenConfig {
     return JSON.parse(fs.readFileSync("./src/TMItokens.json").toString());
+}
+export function getStream(): StreamData {
+    return JSON.parse(fs.readFileSync("./src/commands/builtin/stream.json").toString());
+} 
+export function setStream(data: StreamData): void {
+    fs.truncateSync("./src/commands/builtin/stream.json", 0);
+    fs.writeFileSync("./src/commands/builtin/stream.json", JSON.stringify(data, undefined, 4));
 }
